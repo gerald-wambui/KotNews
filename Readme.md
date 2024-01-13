923409b2bc545f9b3bf634a1ae9709abc1c8f96a
gerald-wambui <gw746071@gmail.com>
2024-01-13 22:27:39 +0300

    test

---
+++ Readme.md
@@ -0,0 +1,1 @@
+hello from kot_news
--- app/src/main/java/com/jaguh/kotnews/MainActivity.kt
+++ app/src/main/java/com/jaguh/kotnews/MainActivity.kt
@@ -22,20 +22,13 @@ class MainActivity : ComponentActivity() {
 					modifier = Modifier.fillMaxSize(),
 					color = MaterialTheme.colorScheme.background
 				) {
-					Greeting("Android")
+
 				}
 			}
 		}
 	}
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-	Text(
-
-		modifier = modifier
-	)
-}
 
 @Preview(showBackground = true)
 @Composable
